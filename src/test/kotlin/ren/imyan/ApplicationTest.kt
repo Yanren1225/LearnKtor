6da8fffa346a1e97700f313d4c0bbae81590eb98
EndureBlaze <hi@imyan.ren>
2021-12-03 18:10:51 +0800

    first

---
+++ src/test/kotlin/ren/imyan/ApplicationTest.kt
@@ -0,0 +1,25 @@
+package ren.imyan
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import ren.imyan.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ configureRouting() }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello World!", response.content)
+            }
+        }
+    }
+}
